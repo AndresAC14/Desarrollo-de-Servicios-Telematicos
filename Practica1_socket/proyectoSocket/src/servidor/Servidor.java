f80a20f94b9e49c9b2c5537c9c5058048211e747
AndresAC14 <andresamo0101@gmail.com>
2023-11-19 20:14:35 +0100

    Servidor con timeout y trama 4

--- Practica1_socket/proyectoSocket/src/servidor/Servidor.java
+++ Practica1_socket/proyectoSocket/src/servidor/Servidor.java
@@ -13,6 +13,8 @@ public class Servidor {
 	private static InetAddress ip;
 	private static int puerto;
 	// Crear variables para reenvio, timeout, retrasos
+	private static final int TIMEOUT = 7000; // Temporizador de 7 segundos para recibir 
+	private static final int MAXTRIES = 3;
 
 	// Información servidor
     private static String nombreServidor = "S1"; // Cambiar en los diferentes pc en los que se ejecute el programa  
@@ -22,56 +24,59 @@ public class Servidor {
 				  
 		try {
 			
-			// Crea socket en el que recibirá mensaje
-			creaSocket();
+			int tries = 0;
+			do{
+				// Crea socket en el que recibirá mensaje
+				creaSocket();
 
-			System.out.println("Socket creado");
+				System.out.println("Socket creado");
 
-			// Cuidado con el tam
-			DatagramPacket recibo1 = new DatagramPacket(new byte[ECHOMAX], ECHOMAX);
-			
-			// Recibe el servidor y redirige el paquete a la hebra
-			// Espera a que llegue el mensaje
-			socket.receive(recibo1);
+				// Cuidado con el tam
+				DatagramPacket recibo1 = new DatagramPacket(new byte[ECHOMAX], ECHOMAX);
 
-			//System.out.println("Paquete recibido, creando hebra...");
+				socket.setSoTimeout(TIMEOUT);
+				try {
+					// Recibe el servidor y redirige el paquete a la hebra
+					// Espera a que llegue el mensaje
+					socket.receive(recibo1);
+					
+				} catch (SocketTimeoutException e) {
+					tries++;
+					System.out.println("Se acabo el tiempo :(");
+				}
 
-			/* DOS OPCIONES
-			 * Sacar el codigo_mensaje de la trama -> if (es t1) mandar a la hebra t1 elseif(es t3) mandar a la hebra t3
-			 * 
-			 * 
-			 * Si no ninguna de esas 2 tramas NO es el servidor aceptado -> Poner como disponibles las credenciales, es decir, 
-			 * 								de la t1 coge idCliente y hacer algo en el fichero
-			 * 
-			 * Poner metodo selector tipo, recibo trama llama al metodo selector y ahi ya eliges
-			 */
+				socket.close(); // Sino peta la hebra al mandar por el mismo puerto
+	
+				Mensaje mensaje1 = new Mensaje();
+				mensaje1.decodificarMensaje(recibo1.getData());
+	
+				boolean servidorAceptado = (mensaje1.getCodigoServidorAceptado() == codigoServidor) 
+										&& (mensaje1.getNombreServidorAceptado().equals(nombreServidor));
+	
+				if(mensaje1.getCodigoMensaje().equals("1_Cliente_Solicita_Credencial")){
+	
+					ServidorThreadTrama1 servidorThread = new ServidorThreadTrama1(recibo1.getData());
+					new Thread(servidorThread).start();
+	
+				}else if(mensaje1.getCodigoMensaje().equals("3_Cliente_Acepta_Credencial") && servidorAceptado){
+	
+					ServidorThreadTrama3 servidorThread = new ServidorThreadTrama3(recibo1.getData());
+					new Thread(servidorThread).start();
+	
+				}else{
+					// NO es el servidor aceptado -> Poner como disponibles las credenciales, es decir, 
+					// coge idCliente y hacer algo en el fichero
+					System.out.println("NO es el servidor aceptado");
+				}	
+	
+				Thread.sleep(2000); // 2 segundos
+				System.out.println("Servidor en espera de que termine de procesarse el mensaje....");
+				
+				Thread.sleep(2000); 
+				System.out.println("Servidor listo para recibir...");
 
-			socket.close(); // Sino peta la hebra al mandar por el mismo puerto
+			}while(tries < MAXTRIES);
 
-			Mensaje mensaje1 = new Mensaje();
-			mensaje1.decodificarMensaje(recibo1.getData());
-
-			boolean servidorAceptado = (mensaje1.getCodigoServidorAceptado() == codigoServidor) 
-                                    && (mensaje1.getNombreServidorAceptado().equals(nombreServidor));
-
-			if(mensaje1.getCodigoMensaje().equals("1_Cliente_Solicita_Credencial")){
-
-				ServidorThreadTrama1 servidorThread = new ServidorThreadTrama1(recibo1.getData());
-				new Thread(servidorThread).start();
-
-			}else if(mensaje1.getCodigoMensaje().equals("3_Cliente_Acepta_Credencial") && servidorAceptado){
-
-				ServidorThreadTrama3 servidorThread = new ServidorThreadTrama3(recibo1.getData());
-				new Thread(servidorThread).start();
-
-			}else{
-				// NO es el servidor aceptado -> Poner como disponibles las credenciales, es decir, 
-				// coge idCliente y hacer algo en el fichero
-			}	
-
-			Thread.sleep(1000); // 1 segundo
-			System.out.println("Servidor en espera de que termine de procesarse el mensaje....");
-			Thread.sleep(1000); // 1 segundo
 
 		} catch (Exception e) {
 			e.printStackTrace();
